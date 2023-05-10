36cdf7631075abdef455424a38447b263eaf20db
TarunMurugan <93092051+TarunMurugan@users.noreply.github.com>
2023-05-10 12:07:13 +0530

    tejas edit

--- ESPControl/ESPControl.ino
+++ ESPControl/ESPControl.ino
@@ -4,9 +4,16 @@
 #error Bluetooth is not enabled! Please run `make menuconfig` to and enable it
 #endif
 
+#define backwardPin 8
+#define stopPin 9
+#define trigPin 10
+
+
+
 BluetoothSerial SerialBT;
 
 String data = "";
+int val = 0;
 int left_raw,right_raw,shoot,leftpin[2]={12,14},rightpin[2]={27,26};
 unsigned long t=0;
 
@@ -29,6 +36,7 @@ void out(int rawval,int pin[2]){
 //right motor pwm:27
 
 void setup() {
+  
   Serial.begin(115200);
   SerialBT.begin("ESP32test"); //Bluetooth device name
   Serial.println("The device started, now you can pair it with bluetooth!");
@@ -36,6 +44,9 @@ void setup() {
   pinMode(14,OUTPUT);
   pinMode(27,OUTPUT);
   pinMode(26,OUTPUT);
+  pinMode(backwardPin, INPUT_PULLUP);
+  pinMode(stopPin, INPUT_PULLUP);
+  pinMode(trigPin, INPUT_PULLUP);
 }
 
 
@@ -64,6 +75,30 @@ out(right_raw,rightpin);
 Serial.print(left_raw); 
 Serial.print(",");
 Serial.print(right_raw);
+
+if (digitalRead(trigPin) == LOW){
+        val = 1;
+    }
+    else if (digitalRead(stopPin) == LOW){
+        val = 0;
+    }
+    
+    if (digitalRead(backwardPin) == LOW){
+        val = -1;
+    }
+
+    if (val == 1){
+        Serial.print("Forward\t");
+    }
+    else if (val == -1){
+        Serial.print("Backward\t");
+    }
+    else if (val == 0){
+        Serial.print("Stop\t");
+    }
+
+    Serial.print(val);
+    Serial.print("\n");
 
 
 delay(50);
